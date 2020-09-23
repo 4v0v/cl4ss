6016553b807c978a30ec262191900a71be4f9987
4v0v <30574115+4v0v@users.noreply.github.com>
2020-09-23 02:26:44 +0200

    Update class.lua

--- class.lua
+++ class.lua
@@ -1,17 +1,15 @@
 local Class = {}
-function Class:new() end
-function Class:extend(className) 
-    local obj = {}
-        obj.__super    = self
-        obj.__class    = className or "Object"
-        obj.__tostring = self.__tostring
-        obj.__call     = self.__call
-        obj.__index    = obj
-    return setmetatable(obj, self)
+
+function Class:extend(name)
+	local obj = {}
+		obj.super   = self
+		obj.class   = function() return name or "Default" end
+		obj.new     = function() end
+		obj.__call  = self.__call
+		obj.__index = obj
+	return setmetatable(obj, self)
 end
-function Class:class() return self.__class end
-function Class:super() return self.__super end
 function Class:__index(v) return Class[v] end
 function Class:__call(...) local obj = setmetatable({}, self) obj:new(...) return obj end
-function Class:__tostring() return self:class() end
+
 return Class
